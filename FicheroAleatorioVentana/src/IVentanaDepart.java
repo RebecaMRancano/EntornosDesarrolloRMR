dfd71c96533d061883be1041cbe33b0632cc2129
RebecaMRancano <rebecamrancano@gmail.com>
2022-03-13 12:39:04 +0100

    Subir Fichero Aleatorio Ventana

---
+++ FicheroAleatorioVentana/src/IVentanaDepart.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+
+public interface IVentanaDepart {
+
+	void ver();
+
+	void modificar();
+
+	void borrar();
+
+	void alta();
+
+	void consultas(ActionEvent e);
+
+}
